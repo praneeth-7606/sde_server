fa62dcab6b8f397930f02a44e44b6762c5b1ee72
praneeth vedagiri <96403254+praneeth-7606@users.noreply.github.com>
2024-08-06 01:10:35 +0530

    Update db.js
    small changes

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 // import colors from "colors"
 const database=async()=>{
     try{
-        const conn =await mongoose.connect("mongodb://localhost:27017/myimages");
+        const conn =await mongoose.connect("mongodb+srv://mynewuser:number1234@cluster0.dzro3qd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
         console.log(`connected to mongodb database`)
     }catch(error){
         console.log(`error in mongodb ${error}`.bgRed.white)
